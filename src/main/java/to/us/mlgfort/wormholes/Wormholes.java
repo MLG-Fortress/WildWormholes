eda31f319b831e8aa762e1e0179e3aafe7a36d0a
RoboMWM <robomwm@users.noreply.github.com>
2018-10-13 16:00:31 -0700

    2.1: add blacklistedWorlds config

--- src/main/java/to/us/mlgfort/wormholes/Wormholes.java
+++ src/main/java/to/us/mlgfort/wormholes/Wormholes.java
@@ -2,7 +2,10 @@ package to.us.mlgfort.wormholes;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created on 5/29/2017.
@@ -16,8 +19,10 @@ public class Wormholes extends JavaPlugin
     //TODO: Store and cleanup old wormholes (in case of server crash)
     public void onEnable()
     {
+        getConfig().addDefault("blacklistedWorlds", Collections.singletonList("spawn"));
+        saveConfig();
         thera = new Thera(this);
-        new WormholeSpawner(this, thera, new HashSet<>()); //TODO: add config for world blacklist
+        new WormholeSpawner(this, thera, new HashSet<>(getConfig().getStringList("blacklistedWorlds")));
         new WormholeTransporter(this, thera);
     }
 
